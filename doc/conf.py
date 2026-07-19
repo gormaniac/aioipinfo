b2e612ef4cfe3cae0105e184e356dc4669657d3c
gormaniac <83027042+gormaniac@users.noreply.github.com>
2026-07-19 00:10:30 -0600

    docs: switch to shibuya theme

--- doc/conf.py
+++ doc/conf.py
@@ -11,8 +11,37 @@ extensions = ["sphinx.ext.autodoc"]
 templates_path = ["_templates"]
 exclude_patterns = []
 
+html_theme = "shibuya"
+html_static_path = ["_static"]
+html_title = "aioipinfo Docs"
 
-html_theme = "sphinx_rtd_theme"
-html_static_path = []
+html_baseurl = 'https://docs.gormo.co/aioipinfo/'
 
-html_baseurl = 'https://gormo.co/docs/caic-python/'
+html_theme_options = {
+    "accent_color": "teal",
+    "color_mode": "auto",
+    "nav_socials": [
+        {
+            "name": "GitHub",
+            "url": "https://github.com/gormaniac/aioipinfo",
+            "icon": "simple-icons:github",
+        },
+        {
+            "name": "PyPI",
+            "url": "https://pypi.org/project/aioipinfo/",
+            "icon": "simple-icons:pypi",
+        },
+    ],
+    "foot_socials": [
+        {
+            "name": "GitHub",
+            "url": "https://github.com/gormaniac/aioipinfo",
+            "icon": "simple-icons:github",
+        },
+        {
+            "name": "PyPI",
+            "url": "https://pypi.org/project/aioipinfo/",
+            "icon": "simple-icons:pypi",
+        },
+    ]
+}
